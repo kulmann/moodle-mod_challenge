2788fec1e36c88f4afd9de879fda1f5717fb07c2
Alexander Dominicus <80880326+Dmfama20@users.noreply.github.com>
2022-03-04 12:06:27 +0100

    Update game.php
    Fix Issue #23

--- classes/model/game.php
+++ classes/model/game.php
@@ -362,16 +362,47 @@ class game extends abstract_model {
 
         // create a set of matches
         $match_number = $round->get_next_match_number();
-        while (count($participants) > 1) {
-            $mdl_user_1 = array_shift($participants);
-            $mdl_user_2 = array_shift($participants);
-            $match = new match();
-            $match->set_mdl_user_1($mdl_user_1->get_mdl_user());
-            $match->set_mdl_user_2($mdl_user_2->get_mdl_user());
-            $match->set_round($round->get_id());
-            $match->set_number($match_number);
-            $match->save();
-        }
+        // fix Issue Issue #23: Check if noumber of participants is even or odd. If it is even, then fine. In odd case:
+            // the first participant (random!) plays a single match and wins the match automatically.
+
+            if(count($participants) % 2 == 0)   {
+                // Case one: number of participants is even
+                while (count($participants) > 1) {
+                    $mdl_user_1 = array_shift($participants);
+                    $mdl_user_2 = array_shift($participants);
+                    $match = new match();
+                    $match->set_mdl_user_1($mdl_user_1->get_mdl_user());
+                    $match->set_mdl_user_2($mdl_user_2->get_mdl_user());
+                    $match->set_round($round->get_id());
+                    $match->set_number($match_number);
+                    $match->save();
+                }
+            }
+            else{
+                // Case two: number of participants is odd
+                    $mdl_user_1 =array_shift($participants);
+                    $mdl_user_2 = $mdl_user_1;
+                    $match = new match();
+                    $match->set_mdl_user_1($mdl_user_1->get_mdl_user());
+                    $match->set_mdl_user_2($mdl_user_2->get_mdl_user());
+                    $match->set_round($round->get_id());
+                    $match->set_number($match_number);
+                    $match->save();
+
+
+                while (count($participants) > 1) {
+                    $mdl_user_1 = array_shift($participants);
+                    $mdl_user_2 = array_shift($participants);
+                    $match = new match();
+                    $match->set_mdl_user_1($mdl_user_1->get_mdl_user());
+                    $match->set_mdl_user_2($mdl_user_2->get_mdl_user());
+                    $match->set_round($round->get_id());
+                    $match->set_number($match_number);
+                    $match->save();
+                }
+
+            }
+        
 
         // track number of created matches in round
         $round->set_matches_created($match_number);
